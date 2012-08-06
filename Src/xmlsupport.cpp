0e6ae51a5eef9ef2b5b73580ca42bbbef99e8ad1
Doug Perkowski <doug@oc3ent.com>
2012-08-06 00:05:41 -0400

    Initial checkin of FxOgreFBX.

---
+++ Src/xmlsupport.cpp
@@ -0,0 +1,25 @@
+//------------------------------------------------------------------------------
+// A wrapper around tinyxml.
+//
+// Owner: Jamie Redmond
+//
+// Copyright (c) 2002-2011 OC3 Entertainment, Inc.
+//------------------------------------------------------------------------------
+/*********************************************************************************
+*                                                                                *
+*   This program is free software; you can redistribute it and/or modify         *
+*   it under the terms of the GNU Lesser General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or            *
+*   (at your option) any later version.                                          *
+*                                                                                *
+**********************************************************************************/
+#include "xmlSupport.h"
+
+#if FXOGREFBX_USE_TINYXML
+
+#include "tinyxml.cpp"
+#include "tinystr.cpp"
+#include "tinyxmlerror.cpp"
+#include "tinyxmlparser.cpp"
+
+#endif // FXOGREFBX_USE_TINYXML
